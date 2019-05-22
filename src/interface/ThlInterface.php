a86fda512012d6754aebef7abef8a50a8f914531
alex <weiwenbin@yayuanzi.com>
2019-05-22 12:15:37 +0800

    feat

---
+++ src/interface/ThlInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 2019/5/22
+ * Time: 下午12:14
+ */
+
+interface ThlInterface
+{
+    /**
+     * 重写返回值
+     *
+     * @param $result
+     * @return mixed
+     */
+    function returnRewrite($result);
+}
